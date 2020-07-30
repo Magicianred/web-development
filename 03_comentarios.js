e5b1d4da88017f4d84e9e260abc1ad03c826e2ef
CamilaBodack <camila.bodack@gmail.com>
2020-07-30 16:58:12 -0300

    fundamentos aulas de estrutura e aula de comentários

---
+++ 03_comentarios.js
@@ -0,0 +1,15 @@
+console.log("Aula sobre comentários")
+console.log("Linha simples")
+// comentarios de de uma linha
+console.log("multiplas linhas jeito ping pong")
+
+/*
+multiplas linhas
+*/
+console.log("multiplas linhas multiplos *")
+/*
+* R
+* Y
+* U
+* K
+*/
---
+++ 03_fundamentos/organizacao.js
@@ -0,0 +1,9 @@
+// o js é organizado por sentenças de código e blocos de código
+
+console.log('Sentença de código')
+{
+  {
+    console.log("Olá")
+    console.log("Ryuk")
+  }
+}
