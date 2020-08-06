7b6d178bc4cbb0710ead42eaa0d6b481e4b8ae4c
CamilaBodack <camila.bodack@gmail.com>
2020-08-06 12:43:02 -0300

    adiciona continue e for in

--- estruturas_controle/for_01.js
+++ estruturas_controle/for_01.js
@@ -10,6 +10,6 @@ for (let i = 1; i <= 10; i++){
 }
 
 const dadosRpg = [1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 18, 20, 24, 30, 34, 50, 60, 100, 120]
-  for(i = 1; i < dadosRpg.length; i++){
-    console.log(`Dados: ${dadosRpg}`)
+  for(let i = 0; i < dadosRpg.length; i++){
+    console.log(`Dado com: ${dadosRpg[i]} lados disponível`)
   }
