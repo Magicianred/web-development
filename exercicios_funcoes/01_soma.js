60dd3c19d29505e525864ed0c2eb62f4f541e4e9
CamilaBodack <camila.bodack@gmail.com>
2020-08-08 16:47:27 -0300

    soma e triangulo

---
+++ exercicios_funcoes/01_soma.js
@@ -0,0 +1,8 @@
+/*
+Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+multiplicação e divisão desses valores.
+*/
+
+
+const soma = (num_1, num_2) => { return num_1 + num_2 }
+console.log(soma(25, 30))
