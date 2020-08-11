d4098f3ef7dddb215f21b24f2b95669434bace99
CamilaBodack <camila.bodack@gmail.com>
2020-08-11 08:12:09 -0300

    calculadora com switch

---
+++ exercicios_funcoes/16_calculadora_switch.js
@@ -0,0 +1,29 @@
+/*
+Utilizando a estrutura do Switch faça um programa que simule uma calculadora
+básicaO programa recebe como parâmetros dois valores numéricos e uma string
+referente à operação e a realize com os valoresnuméricos na ordem que foram
+inseridos. Por exemplo: calculadora (2, ‘+’, 3).
+A função efetuará a soma de 2 e 3. Dica: Os sinais das operações são: ‘+’. ‘-’, ‘*’ e ‘/’. Crie um caso default para operações inválidas.
+*/
+
+
+const calculadora = function(a, b, operacao){
+  switch(operacao){
+    case "+":
+      return (a + b)
+    break
+    case "-":
+      return(a - b)
+    break
+    case "/":
+      return(a / b)
+    break
+    case "*":
+      return(a * b)
+    break
+    default:
+      return("Operação não suportada")
+  }
+}
+
+console.log(calculadora(5, 8, "+"))
