566c7d4c503adabf2760d77b5943f8eb2e0199cf
CamilaBodack <camila.bodack@gmail.com>
2020-08-19 18:05:48 -0300

    media do array

---
+++ exercicios_funcoes/32_media_interior_array.js
@@ -0,0 +1,18 @@
+/*
+Construir um algoritmo que calcule a média aritmética dos valores de um vetor de inteiros.
+*/
+
+const mediaIntVetor = function(vetor){
+      let total = 0
+      let contaInt = 0
+
+      for(let i = 0; i < vetor.length; i++){
+          if(Number.isInteger(vetor[i])){
+            total += vetor[i]
+            contaInt++
+          }
+      }
+      return 'Média dos valores do array: ' + (total/contaInt)
+}
+
+console.log(mediaIntVetor([6,8,10]))
