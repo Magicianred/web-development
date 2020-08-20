9dc5606e9bc43974a9fb82851ea863e48375a49c
CamilaBodack <camila.bodack@gmail.com>
2020-08-20 09:57:22 -0300

    finaliza comparacao dos arrays de caracteres

--- exercicios_funcoes/34_caracteres_em_palavras.js
+++ exercicios_funcoes/34_caracteres_em_palavras.js
@@ -3,18 +3,23 @@ Construa uma função que receberá duas Strings de tamanhos variados e que reto
 todos os caracteres (independentemente de ser maiúsculo ou minúsculo) estejam contidos em ambas palavras.
 */
 let string_1 = 'Camila'
-let string_2 = 'Camila'
+let string_2 = 'Siberia'
+
 const verificaCaracter = function(string_1, string_2){
-    let gera_array_char = Array.from(string_1)
-        for(let i =0; i < string_2.length; i++){
-            if(string_2[i] in gera_array_char){
-              return true
-            }
-            else{
-              return 'false'
-            }
+    let array_string_1 = string_1.toLowerCase().split("")
+    let array_string_2 = string_2.toLowerCase().split("")
+
+    conta_char = 0
+    for(let i = 0; i < array_string_2.length; i++){
+        if(array_string_1.includes(array_string_2[i])){
+          conta_char++
         }
-    }
+      }if(conta_char == array_string_2.length){
+          return true
+      }else{
+          return false
+      }
+}
 
 
 console.log(verificaCaracter(string_1, string_2))
