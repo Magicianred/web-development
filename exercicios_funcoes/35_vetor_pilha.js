1e4df890d102c279166498c6824e7e513ff3cbbd
CamilaBodack <camila.bodack@gmail.com>
2020-08-20 10:22:53 -0300

    vetor pilha + vetor adiciona via spread

---
+++ exercicios_funcoes/35_vetor_pilha.js
@@ -0,0 +1,16 @@
+/*
+Crie dois vetores chamados vetorPilha e vetorAdiciona. Inicialmente, o vetorPilha conterá cinco elementos
+inteiros: [1, 2, 3, 4, 5]. Você deverá adicionar os valores contidos no vetorAdiciona [6, 7, 8, 9, 10] ao vetor pilha
+e mostrá-los no console. É importante lembrar que o método Push retorna somente o tamanho do Vetor. Ao
+final das operações imprima os vetores no console.
+*/
+
+let vetorPilha = [1, 2, 3, 4, 5]
+let vetorAdiciona = [6, 7, 8, 9, 10]
+
+const adicionarVetor = function(vetorPilha, vetorAdiciona){
+    let une_vetores = [...vetorPilha,...vetorAdiciona]
+    return 'Vetores: ' + une_vetores
+}
+
+console.log(adicionarVetor(vetorPilha, vetorAdiciona))
