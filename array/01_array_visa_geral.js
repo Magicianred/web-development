4d339ec3c95f272808d013ba8d9863d04aba963b
CamilaBodack <camila.bodack@gmail.com>
2020-08-22 09:23:40 -0300

    adiciona secao array

---
+++ array/01_array_visa_geral.js
@@ -0,0 +1,9 @@
+/*
+Array em js é um objeto
+Ideal é trabalhar com dados homogênios
+forma ideal de criação é a forma literal
+*/
+
+console.log(typeof Array)
+console.log(typeof new Array)
+console.log(typeof [])
