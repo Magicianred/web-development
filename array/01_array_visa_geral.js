bc981273ea7055ec964a5e7573c4f477674142e9
CamilaBodack <camila.bodack@gmail.com>
2020-08-22 09:35:54 -0300

    array visao geral

--- array/01_array_visa_geral.js
+++ array/01_array_visa_geral.js
@@ -7,3 +7,13 @@ forma ideal de criação é a forma literal
 console.log(typeof Array)
 console.log(typeof new Array)
 console.log(typeof [])
+
+wands = ['wand of cobra', 'wand of dimensions', 'wand of energy']
+
+wands.sort()
+console.log(wands)
+
+delete wands[1]
+console.log(wands)
+wands.splice(1, 0, 'blossom staff', 'wand of tainha') // insere nas posicoes 1 e 0 do array os itens
+console.log(wands)
