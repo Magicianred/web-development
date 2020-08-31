7be39920e0f3b7eae5008546b3fe6d01acd960ce
CamilaBodack <camila.bodack@gmail.com>
2020-08-31 18:51:33 -0300

    tratar error

---
+++ esnext/tratar_erro_01.js
@@ -0,0 +1,12 @@
+function funcionarOuNao(valor, chanceErro){
+    return new Promise((resolve, reject) => {
+        if(Math.random() < chanceErro){
+        reject('Ocorreu um erro')
+    }else{
+        resolve(valor)
+        }
+    })
+}
+funcionarOuNao('Teste', 0.9)
+.then(console.log)
+.catch(err => console.log(`${err}`))
