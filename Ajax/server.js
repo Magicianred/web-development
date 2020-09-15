68592b809f30fed61e1d0877287edc22e33dc6c5
CamilaBodack <camila.bodack@gmail.com>
2020-09-15 10:52:23 -0300

    adicionado form com fetch

--- Ajax/server.js
+++ Ajax/server.js
@@ -6,5 +6,10 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-
+app.post('/formulario', (req, res) => {
+	res.send({
+		...res.body,
+		id: 1
+	})
+})
 app.listen(8080, () => console.log('funcionando'))
